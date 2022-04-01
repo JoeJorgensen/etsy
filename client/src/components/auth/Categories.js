f91e2970745e78dc48f5a2c63ad98297b169dc1d
JoeJorgensen <joebjorg@gmail.com>
2022-04-01 14:47:20 -0600

    UI

--- client/src/components/auth/Categories.js
+++ client/src/components/auth/Categories.js
@@ -1,13 +1,12 @@
+import Badge from "react-bootstrap/esm/Badge"
 
 
 const Categories = ()=>{
 
 
-
-    
     return(
-        <div>
-            <h1>Categories</h1>
+        <div style={{textAlign:'center'}}>
+            <Badge bg='dark' style={{margin:'10px'}}><h1>Categories</h1></Badge>
 
         </div>
     )
